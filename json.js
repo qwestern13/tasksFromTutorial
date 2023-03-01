619c1335296625f18526dc3d14d535087385f3ca
qwestern13 <qwestern13@yandex.ru>
2023-03-01 15:45:53 +0500

    added classes and json

---
+++ json.js
@@ -0,0 +1,23 @@
+let user = {
+    name: "Василий Иванович",
+    age: 35
+};
+  
+let user2 = JSON.parse(JSON.stringify(user));
+
+let room = {
+    number: 23
+};
+  
+let meetup = {
+    title: "Совещание",
+    occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+    place: room
+};
+  
+room.occupiedBy = meetup;
+meetup.self = meetup;
+  
+console.log(JSON.stringify(meetup, function replacer(key, value) {
+    return (key != "" && value == meetup) ? undefined : value;
+}));
